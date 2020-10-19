97730ab15428f8c789abec2ec34c1a26c34cf7c3
Bhupender Kumar Saini <bksaini078@gmail.com>
2020-10-20 04:59:27 +0530

    changingapp name

--- evaluation.py
+++ evaluation.py
@@ -38,93 +38,19 @@ MT_synonym= load_model ( 'models/MT_synonym')
 MT_unlabel=load_model ( 'models/MT_unlabel')
 MT_dropout=load_model ( 'models/MT_dropout')
 MT_synanddrop=load_model ( 'models/MT_synanddrop')
-VAT=load_model ( 'models/VAT_regularization')
-Psuedo_Label=load_model ( 'models/Psuedo_Label')
 
 
-# this is only for vat model
-def load_emb_matrix(path) :
-    """
-    Loads the embedding format stored locally as npy format
-    :return: embedding_matrix
-    """
-    embedding_matrix = np.load( meta_path+'/emb_matrix.npy' )
-    # print ( 'Embedding matrix load from local sys successful\n' )
-    # print (
-    #     'Denormalized Shape: ({},{})'.format ( np.shape ( embedding_matrix )[0], np.shape ( embedding_matrix )[1] ) )
-    return embedding_matrix
 
-def embedding(seq, matrix):
-    emb_dim = 300
-    doc_length = 100
-    # input_dim is +1 because word_index starts from index 1 and embedding matrix from 0
-    word_index = open ( meta_path+'/word_index.pickle', 'rb' )
-    word_index = pickle.load ( word_index )
-    emb = Embedding(input_dim=len(word_index) + 1,
-                    output_dim=emb_dim,
-                    input_shape=(doc_length,),
-                    weights=[matrix],
-                    trainable=False,
-                    mask_zero=True)(seq)
-    return emb
-def prec_rec_f1_score_vat(model, x, y_true) :
-    from sklearn.metrics import precision_recall_fscore_support
-    from sklearn.metrics import accuracy_score
-
-    y_hat = model.predict ( x )
-    y_pred = np.argmax ( y_hat, axis=1 )
-    pr_re_f1score_perclass = precision_recall_fscore_support ( y_true, y_pred )
-    accuracy = accuracy_score ( y_true, y_pred )
-    # per class
-    precision_true = round ( pr_re_f1score_perclass[0][1], 3 )
-    precision_fake = round ( pr_re_f1score_perclass[0][0], 3 )
-    recall_true = round ( pr_re_f1score_perclass[1][1], 3 )
-    recall_fake = round ( pr_re_f1score_perclass[1][0], 3 )
-    f1score_true = round ( pr_re_f1score_perclass[2][1], 3 )
-    f1score_fake = round ( pr_re_f1score_perclass[2][0], 3 )
-    metrics_name = ['accuracy', 'precision_true', 'precision_fake', 'recall_true', 'recall_fake', 'f1score_true',
-                    'f1score_fake']
-    metrics_value = [accuracy, precision_true, precision_fake, recall_true, recall_fake, f1score_true, f1score_fake]
-    i = 0
-    # for item in metrics_name :
-    #     print ( item + ':', metrics_value[i] )
-    #     i += 1
-    loss_object = tf.keras.losses.SparseCategoricalCrossentropy ( from_logits=False )
-    loss = loss_object ( y_true, y_hat ).numpy ()
-    # print ( 'loss', loss )
-    fpr_, tpr_, _ = roc_curve ( y_true, y_pred )
-    return accuracy, precision_true, precision_fake, recall_true, recall_fake, f1score_true, f1score_fake,\
-           round ( loss,3 ),fpr_, tpr_, y_pred
-
-
-def test_vat(y_true, x_test, model_vat) :
-    embedding_matrix = load_emb_matrix ( meta_path+'/emb_matrix.npy' )
-
-    # test_feature, test_label = next ( iter ( test_dataset ) )
-    test_emb = embedding( x_test, embedding_matrix )
-    # print ( 'Test dataset batch size: ', test_emb.shape, '\n' )
-    # model_vat.evaluate ( test_emb, test_label )
-    # print ( '\n' )
-
-    test_accuracy, precision_true, precision_fake, recall_true, recall_fake, \
-    f1score_true, f1score_fake, loss,fpr_, tpr_, y_pred  = prec_rec_f1_score_vat( model_vat, test_emb, y_true )
-    # print ( '*' 55, 'Done', '*'55)
-
-    return test_accuracy, precision_true, precision_fake, recall_true, recall_fake, \
-           f1score_true, f1score_fake, loss,fpr_, tpr_, y_pred
 # model_vat is supposed to be the last model zip file I sent
 # call test_vat()
 def prec_rec_f1score(y_true, x_test, model,item) :
     bce = tf.keras.metrics.BinaryCrossentropy()
     # print(model.summary() )
     y_hat = model.predict(x_test)
-
     y_pred = (np.greater_equal( y_hat, 0.505 )).astype ( int )
     # for psuedo labelling and Vat technique
     # print(item+'********')
-    if item =='Psuedo_Label':# or item=='VAT_regularization':
-        y_pred=tf.argmax(y_hat,1).numpy()
-        # y_hat= np.max(y_hat,axis=1)# this one for calculating binary loss
+    # y_hat= np.max(y_hat,axis=1)# this one for calculating binary loss
     # print(y_hat)
     # print(y_pred)
     pr_re_f1score_perclass = precision_recall_fscore_support (y_true, y_pred, average=None )
@@ -142,8 +68,6 @@ def prec_rec_f1score(y_true, x_test, model,item) :
     f1score_fake = pr_re_f1score_perclass[2][0]
 
     fpr_, tpr_, _ = roc_curve( y_true, y_pred)
-    if item == 'Psuedo_Label':# or item == 'VAT_regularization' :
-        y_true = tf.one_hot ( y_true, 2 ).numpy ()
     binary_loss = bce ( y_true, y_hat ).numpy ()
     return accuracy, precision_true, precision_fake, recall_true, recall_fake, f1score_true, f1score_fake, binary_loss,fpr_, tpr_, y_pred
 # tokenizer function
@@ -158,14 +82,11 @@ def tokenize(x_test):
     return x_test
 
 def model_evaluation(df) :
-
     folder_name = os.listdir ( 'models' )
     folder_name.pop( 0 )
     x_test = (df['Article'].astype ( str )).values
     df['Label'] = df['Label'].astype (int)
     y_test = (df['Label'].astype ( int )).values
-    x_test_vat= np.load('meta_vat/x_test_vat.npy',allow_pickle=True)
-    y_test_vat= np.load('meta_vat/y_test_vat.npy',allow_pickle=True)
     x_test = tokenize (x_test)
     # creating dataframe
     report_df = pd.DataFrame(columns=['Model', 'Accuracy', 'Precision_True', 'Precision_Fake', 'Recall_True',
@@ -176,13 +97,8 @@ def model_evaluation(df) :
     for items in folder_name :
         print(items)
         model = load_model ( 'models/' + items)
-        if items == 'VAT_regularization':
-            # x_test = tokenize ( x_test )
-            accuracy, precision_true, precision_fake, recall_true, recall_fake, f1score_true, \
-            f1score_fake, binary_loss, fpr, tpr, y_pred = test_vat( y_test_vat[:no_row], x_test_vat[:no_row], model)
-        else:
-            accuracy, precision_true, precision_fake, recall_true, recall_fake, f1score_true, \
-            f1score_fake, binary_loss, fpr, tpr,y_pred = prec_rec_f1score( y_test, x_test, model,items)
+        accuracy, precision_true, precision_fake, recall_true, recall_fake, f1score_true, \
+        f1score_fake, binary_loss, fpr, tpr,y_pred = prec_rec_f1score( y_test, x_test, model,items)
         predict_df[items]= y_pred
         # print('before pyplot')
         pyplot.plot(fpr, tpr, linestyle='--', label=items )
@@ -213,29 +129,14 @@ def model_prediction(article):
     predict_df = pd.DataFrame()
     predict_df['Article']= [article]
     print(MT_synonym.predict(article_seq))
-
     y_hat=((MT_synonym.predict(article_seq)).round())
     predict_df['MT_synonym'] = (np.greater_equal ( y_hat, 0.505 ))
-
-
     y_hat = ((MT_dropout.predict ( article_seq )).round ())
     predict_df['MT_dropout'] = (np.greater_equal ( y_hat, 0.505 ))
-
     y_hat = ((MT_synanddrop.predict ( article_seq )).round ())
     predict_df['MT_synanddrop'] = (np.greater_equal ( y_hat, 0.505 ))
-
     y_hat = ((MT_unlabel.predict ( article_seq )).round ())
     predict_df['MT_Unlabel'] = (np.greater_equal ( y_hat, 0.505 ))
-
-    # y_vat= (VAT.predict(article_seq))
-    # predict_df['VAT_regularization']= np.argmax( y_vat, axis=1 )
-    y_p= Psuedo_Label.predict(article_seq)
-    predict_df['Psuedo_label'] = bool (np.argmax( y_p, axis=1 ))
-    embedding_matrix = load_emb_matrix ( meta_path+'/emb_matrix.npy' )
-    # test_feature, test_label = next ( iter ( test_dataset ) )
-    test_emb = embedding( article_seq, embedding_matrix )
-    y_vat = (VAT.predict ( test_emb ))
-    predict_df['VAT regularization'] = bool ( np.argmax ( y_vat, axis=1 ) )
     predict_df.replace({False:'Fake'})
     return predict_df
 def model_prediction_df(df):
@@ -250,13 +151,6 @@ def model_prediction_df(df):
     predict_df['MT_dropout'] =  ( (MT_dropout.predict ( x_test_seq )).round () ).astype(bool)
     predict_df['MT_Unlabel'] = ( (MT_unlabel.predict ( x_test_seq )).round () ).astype(bool)
     predict_df['MT_Synanddrop'] = ( (MT_synanddrop.predict ( x_test_seq )).round () ).astype(bool)
-    y_p = Psuedo_Label.predict ( x_test_seq )
-    predict_df['Psuedo_label'] =  ( np.argmax ( y_p, axis=1 ) ).astype(bool)
-    embedding_matrix = load_emb_matrix ( meta_path + '/emb_matrix.npy' )
-    # test_feature, test_label = next ( iter ( test_dataset ) )
-    test_emb = embedding ( x_test_seq, embedding_matrix )
-    y_vat = (VAT.predict ( test_emb ))
-    predict_df['VAT regularization'] =  ( np.argmax ( y_vat, axis=1 ) ).astype(bool)
     predict_df.replace( {False : 'Fake'} )
     return predict_df
 
